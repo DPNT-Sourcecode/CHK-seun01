69e2a8ef15a6329c07b5f8eaf8e7fd817cb70bfe
sbx_user1051 <sbx_user1051@169.254.94.255>
2026-03-13 00:37:40 +0000

    Fri Mar 13 00:37:40 UTC 2026

--- src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,23 @@
+package io.accelerate.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class HelloSolutionTest {
-  
+
+    private HelloSolution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new HelloSolution();
+    }
+
+    @Test
+    void testItReturnsHello() {
+        String response = solution.hello("Jorge");
+        assertEquals("Hello Jorge", response);
+    }
+
 }
