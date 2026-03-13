cebaeee9bf755d302e6d8533a1b7c014aae95e91
sbx_user1051 <sbx_user1051@169.254.94.255>
2026-03-13 01:04:54 +0000

    Fri Mar 13 01:04:54 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,73 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+record SpecialOffer(
+        int quantity,
+        Integer offerPrice
+) {
+}
+
+class Item {
+
+    private final Integer price;
+    private final SpecialOffer specialOffer;
+
+    public Item(Integer price, SpecialOffer specialOffer) {
+        this.price = price;
+        this.specialOffer = specialOffer;
+    }
+
+    public Item(Integer price) {
+        this.price = price;
+        this.specialOffer = null;
+    }
+
+    public Integer calculateFinalPrice(int quantity) {
+        if (quantity == 0) {
+            return 0;
+        }
+
+        if (specialOffer == null || quantity < specialOffer.quantity()) {
+            return quantity * price;
+        }
+
+        int quantityAboveOffer = quantity - specialOffer.quantity();
+
+        return specialOffer.offerPrice() + (quantityAboveOffer * price);
+    }
+}
+
+
 public class CheckoutSolution {
+
+    private Map<String, Item> priceTable = new HashMap<>();
+
+    public CheckoutSolution() {
+        priceTable.put("A", new Item(50, new SpecialOffer(3, 130)));
+        priceTable.put("B", new Item(30, new SpecialOffer(2, 45)));
+        priceTable.put("C", new Item(20));
+        priceTable.put("D", new Item(15));
+    }
+
     public Integer checkout(String skus) {
+        if (skus == null || skus.isBlank()) {
+            return -1;
+        }
+
+        List<String> skusList = Arrays.stream(skus.split(",")).toList();
+        Integer basketPrice = skusList.stream()
+                .map(sku -> priceTable.get(sku))
+                .reduce(0, )
+
+
+
         throw new SolutionNotImplementedException();
     }
 }
+
