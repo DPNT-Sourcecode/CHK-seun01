4a7af2211ba120fac2bd223bf22dee92ad5d096a
sbx_user1051 <sbx_user1051@169.254.35.225>
2026-03-18 00:25:19 +0000

    Wed Mar 18 00:25:19 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -65,6 +65,37 @@ class Item {
 class Store {
     private static final List<String> VALID_SKUS = List.of("A", "B", "C", "D", "E", "F");
 
+    +------+-------+------------------------+
+            | Item | Price | Special offers         |
+            +------+-------+------------------------+
+            | A    | 50    | 3A for 130, 5A for 200 |
+            | B    | 30    | 2B for 45              |
+            | C    | 20    |                        |
+            | D    | 15    |                        |
+            | E    | 40    | 2E get one B free      |
+            | F    | 10    | 2F get one F free      |
+            | G    | 20    |                        |
+            | H    | 10    | 5H for 45, 10H for 80  |
+            | I    | 35    |                        |
+            | J    | 60    |                        |
+            | K    | 80    | 2K for 150             |
+            | L    | 90    |                        |
+            | M    | 15    |                        |
+            | N    | 40    | 3N get one M free      |
+            | O    | 10    |                        |
+            | P    | 50    | 5P for 200             |
+            | Q    | 30    | 3Q for 80              |
+            | R    | 50    | 3R get one Q free      |
+            | S    | 30    |                        |
+            | T    | 20    |                        |
+            | U    | 40    | 3U get one U free      |
+            | V    | 50    | 2V for 90, 3V for 130  |
+            | W    | 20    |                        |
+            | X    | 90    |                        |
+            | Y    | 10    |                        |
+            | Z    | 50    |                        |
+            +------+-------+------------------------+
+
     private final Map<String, Item> priceTable = new HashMap<>();
 
     public Store() {
@@ -185,4 +216,5 @@ public class CheckoutSolution {
         return amountBySku;
     }
 }
+
 
